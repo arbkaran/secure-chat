2a769c6563f52efbf7fcdecc52aa35debdc13ac6
arb karan <karabarb1@gmail.com>
2026-07-31 16:27:18 +0800

    fix: import FileSystem from expo-file-system/legacy to avoid deprecation errors

--- frontend/screens/ChatScreen.js
+++ frontend/screens/ChatScreen.js
@@ -15,7 +15,7 @@ import {
 import * as ImagePicker from 'expo-image-picker';
 import * as DocumentPicker from 'expo-document-picker';
 import * as Sharing from 'expo-sharing';
-import * as FileSystem from 'expo-file-system';
+import * as FileSystem from 'expo-file-system/legacy';
 import * as SecureStore from 'expo-secure-store';
 
 import ScreenContainer from '../components/ScreenContainer';
